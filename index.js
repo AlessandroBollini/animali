325c07aa67a900115100dee01412a8c062bcd406
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2023-06-23 16:39:33 +0200

    m

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express=require("express");
 const app=express();
 const port=process.env.PORT || 3000;
 
-const animals=["Leoncina","Zebra","Polpetta","Gamberetta","Crocchetta","Panda","Pinguina","Otaria","Pangolina","Petaura","Bananina","Puzzola"];
-const attributes=["generosa","pucciosa","urlante","tenerona","croccola","delmiocuore","brianzola","bellissima","brontolona","pucciosa","delmcdonald","super","profumata"];
+const animals=["Leoncina","Zebra","Polpetta","Gamberetta","Crocchetta","Panda","Pinguina","Otaria","Pangolina","Petaura","Bananina","Puzzola","Gelosona","Sofficina","Cinghialotta","Volpetta","McNuggets","Giraffina","Birretta","Sgrengkts","Triglia"];
+const attributes=["generosa","pucciosa","urlante","tenerona","croccola","delmiocuore","brianzola","bellissima","brontolona","pucciosa","delmcdonald","super","profumata","chiacchierona","spaziale","calda","alcioccolato","simpatica","divertente","abbracciosa","perfetta","tra le nuvole"];
 
 function rand(){
-    return Math.floor(Math.random() * 13);
+    return Math.floor(Math.random() * 21);
 }
 
 app.get('/',(_req,res)=>{
